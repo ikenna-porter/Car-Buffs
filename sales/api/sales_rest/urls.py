3c6b662fad7f244bc8fd8bef0c608a28aedcff0a
Josh <thejoulesberg@gmail.com>
2022-09-12 17:34:07 -0500

    created jsx files for sales frontend

---
+++ sales/api/sales_rest/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .api_views import api_cars_all,api_cars_for_sale,api_customers,api_sales,api_sales_reps
+
+
+urlpatterns = [
+    path("transactions/",api_sales,name="transactions"),
+    path("reps/",api_sales_reps,name="representatives"),
+    path("customers/",api_customers,name="customers"),
+    path("cars/",api_cars_all,name="cars"),
+    path("cars/forsale",api_cars_for_sale,name="for_sale"),
+]
