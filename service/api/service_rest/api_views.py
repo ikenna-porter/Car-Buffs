f5422dfa045dd734e62a7fc755c49ec62b27f5df
Ikenna <ikenna.porter@gmail.com>
2022-09-13 22:27:27 +0200

    Working on list view

--- service/api/service_rest/api_views.py
+++ service/api/service_rest/api_views.py
@@ -1,7 +1,7 @@
 from django.views.decorators.http import require_http_methods
 from .models import Appointment, Technician, AutomobileVO, Service
 from django.http import JsonResponse
-from common.json import ModelEncoder
+from common.json import ModelEncoder, DateEncoder
 import json
 
 
@@ -34,16 +34,20 @@ class AppointmentsListEncoder(ModelEncoder):
     def get_extra_data(self, o):
         return {"href": o.get_api_url()}
 
-    encoders = [
-        {"technician" : TechnicianEncoder(), 
+    encoders = {
+        "technician" : TechnicianEncoder(), 
         "service": ServiceEncoder(), 
-        "automobile": AutomobileVOEncoder()}
-    ]
+        "automobile": AutomobileVOEncoder(),
+        "date": DateEncoder(),
+        }
+
+
 
 @require_http_methods(["GET"])
 def service_list(request):
     service = Service.objects.all()
     return JsonResponse({"service": service}, encoder=ServiceEncoder, safe=False)
+
 
 @require_http_methods(["GET"])
 def automobile_list(request):
@@ -54,26 +58,35 @@ def automobile_list(request):
 @require_http_methods(["POST", "GET"])
 def list_technicians(request):
     if request.method == "POST":
-        content = json.dumps(request.body)
-        Technician.objects.create(**content)
-        return JsonResponse({"message": "success"})
+        content = json.loads(request.body)
+        technician = Technician.objects.create(**content)
+        return JsonResponse(technician, encoder=TechnicianEncoder, safe=False)
     else:
         technicians = Technician.objects.all()
         return JsonResponse({"technicians": technicians}, encoder=TechnicianEncoder, safe=False)
+
 
 @require_http_methods(["GET", "POST"])
 def list_appointments(request):
     if request.method == "GET":
         appointments = Appointment.objects.all()
-        return JsonResponse({"appointments": appointments}, encoder=AppointmentsListEncoder)
+        return JsonResponse(appointments, encoder=AppointmentsListEncoder, safe=False)
     else: #POST
-        content = json.laods(request.body)
+        content = json.loads(request.body)
+
+        content["automobile"] = AutomobileVO.objects.get(vin=content["automobile"])
+        content["technician"] = Technician.objects.get(employee_id=content["technician"])
+        content["service"] = Service.objects.get(name=content["service"])
+
         Appointment.objects.create(**content)
+
         return JsonResponse({"message": "appointment created"})
+
 
 @require_http_methods
 def appointment_detail():
     pass
+
 
 @require_http_methods
 def list_services():
